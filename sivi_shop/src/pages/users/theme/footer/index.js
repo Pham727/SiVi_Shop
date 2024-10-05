1670b98dc5ede8e2e81c1b97917b9a43b061a2ed
HaPham <ngoha.pham1202@gmail.com>
2024-10-05 16:48:27 +0700

    Add header and footer.

--- sivi_shop/src/pages/users/theme/footer/index.js
+++ sivi_shop/src/pages/users/theme/footer/index.js
@@ -1,7 +1,85 @@
 import { memo } from 'react';
+import "./style.scss";
+import { Link } from "react-router-dom";
+import { AiOutlineFacebook, AiOutlineInstagram, AiOutlineTwitter, AiOutlineLinkedin } from "react-icons/ai";
+
 
 const Footer = () => {
-    return <h1> Footer </h1>
+    return (
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-3 col-md-6 col-sm-6 col-xs-12">
+                        <div className="footer_about">
+                            <h1 className="footer_about_logo">SiVi SHOP</h1>
+                            <ul>
+                                <li>Địa chỉ: số 10 Phổ Quang, Tân Bình</li>
+                                <li>Phone: 0977-232-555</li>
+                                <li>Email: fujinet@gmail.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                        <div className="footer__widget">
+                            <h6>Cửa hàng</h6>
+                            <ul>
+                                <li>
+                                    <Link to="">Liên hệ</Link>
+                                </li>
+                                <li>
+                                    <Link to="">Thông tin về chúng tôi</Link>
+                                </li>
+                                <li>
+                                    <Link to="">Sản phẩm kinh doanh</Link>
+                                </li>
+                                
+                            </ul>
+                            <ul>
+                                <li>
+                                    <Link to="">Thông tin tài khoản</Link>
+                                </li>
+                                <li>
+                                    <Link to="">Giỏ hàng</Link>
+                                </li>
+                                <li>
+                                    <Link to="">Danh sách ưa thích</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col-lg-3 col-md-12 col-sm-12 col-xs-12">
+                        <div className="footer__widget">
+                            <h6>Khuyến mãi & ưu đãi</h6>
+                            <p>Đăng kí nhận thông tin tại đây</p>
+                            <form action="#">
+                                <div className="input-group">
+                                    <input type="text" placeholder="Nhập email"/>
+                                    <button type="submit" className="button-submit">
+                                        Đăng ký
+                                    </button>
+                                </div>
+                                <div className="footer_widget_social">
+                                    <div> 
+                                        <AiOutlineFacebook />
+                                    </div>
+                                    <div> 
+                                        <AiOutlineInstagram />
+                                    </div>
+                                    <div> 
+                                        <AiOutlineTwitter />
+                                    </div>
+                                    <div> 
+                                        <AiOutlineLinkedin />
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </footer>
+    )
 }
 
 export default memo(Footer);
--- sivi_shop/src/pages/users/theme/header/index.js
+++ sivi_shop/src/pages/users/theme/header/index.js
@@ -1,7 +1,202 @@
-import { memo } from 'react';
+import { memo, useState } from 'react';
+import "./style.scss";
+import { ROUTERS } from 'utils/router';
+import { formater } from "utils/formater";
+import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaRegUser } from "react-icons/fa";
+import { AiOutlineMail, AiOutlineShoppingCart, AiOutlineMenu, AiOutlinePhone } from "react-icons/ai";
+import { Link } from "react-router-dom"
 
 const Header = () => {
-    return <h1> Header </h1>
+    const [isShowCategories, setShowCategories] =useState(true);
+    const [menu, setMenu] = useState([
+        {
+            name:"Trang chủ",
+            path: ROUTERS.USER.HOME,
+        },
+        {
+            name:"Cửa hàng",
+            path: ROUTERS.USER.PRODUCTS,
+        },
+        {
+            name:"Sản phẩm",
+            path: "",
+            isShowSubmenu: false,
+            child:[
+                {
+                    name:"Thịt",
+                    path: "",
+                },
+                {
+                    name:"Rau củ",
+                    path: "",
+                },
+                {
+                    name:"Thức ăn nhanh",
+                    path: "",
+                },
+            ]
+        },
+        {
+            name:"Bài viết",
+            path: "",
+        },
+        {
+            name:"Liên hệ",
+            path: "",
+        },
+
+    ]);
+    return (
+        <>
+            <div className="header_top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-6 header_top_left ">
+                            <ul>
+                                <li><AiOutlineMail />ngocha.pham1202@gmail.com
+                                </li>
+                                <li>Mien phi ship don hang tu {formater(200000)}</li>
+                            </ul>
+                        </div>
+                        <div className="col-6 header_top_right">
+                            <ul>
+                                <li> 
+                                    <Link to={""}>
+                                        <FaFacebook />
+                                    </Link>
+                                </li>
+                                <li> 
+                                    <Link to={""}>
+                                        <FaInstagram />
+                                    </Link>
+                                </li>
+                                <li> 
+                                    <Link to={""}>
+                                        <FaLinkedin />
+                                    </Link>
+                                </li>
+                                <li> 
+                                    <Link to={""}>
+                                        <FaTwitter />
+                                    </Link>
+                                </li>
+                                <li> 
+                                    <Link to={""}>
+                                        <FaRegUser />
+                                    </Link>
+                                    <span>Dang nhap</span>
+
+                                </li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row">
+                    <div className="col-xl-3">
+                        <div className="header_logo">
+                            <h1>SiVi SHOP</h1>
+                        </div>
+                    </div>
+                    <div className="col-xl-6">
+                        <nav className="header_menu">
+                            <ul>
+                                {menu.map((menu, menuKey) => (
+                                    <li key={menuKey} className={menuKey === 0 ? "active" : ""}>
+                                        <Link to={menu.path}>{menu.name}</Link>
+                                        {
+                                            menu.child && (
+                                                <ul className="header_menu_dropdown">
+                                                    {menu.child.map((childItem, childKey) => (
+                                                        <li key={`${menuKey} - ${childKey}`}>
+                                                            <Link to={childItem.path}>{childItem.name}</Link>
+                                                        </li>
+                                                    ))}
+                                                </ul>
+                                            )
+                                        }
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-xl-3">
+                        <div className="header_cart">
+                            <div className="header_cart_price">
+                                <span>{formater(1001230)}</span>
+                            </div>
+                            <ul>
+                                <li>
+                                    <Link to="#">
+                                        <AiOutlineShoppingCart /><span>5</span>
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
+                <div className="row hero_categories_container">
+                    <div className="col-lg-3 hero_categories">
+                        <div className="hero_categories_all" onClick={() => setShowCategories(!isShowCategories)}>
+                            <AiOutlineMenu/>
+                            Danh mục sản phẩm
+                        </div>
+                        {isShowCategories && (
+                            <ul className={isShowCategories ? "" : "hidden"}>
+                            <li>
+                                <Link to="">Thịt tươi</Link>
+                            </li>
+                            <li>
+                                <Link to="">Rau củ</Link>
+                            </li>
+                            <li>
+                                <Link to="">Trái cây</Link>
+                            </li>
+                            <li>
+                                <Link to="">Hải sản</Link>
+                            </li>
+                        </ul>
+                        )}
+                    </div>
+                    <div className="col-lg-9 hero_search_container">
+                        <div className="hero_search">
+                            <div className="hero_search_form">
+                                <form>
+                                    <input type="text" placeholder="Bạn đang tìm gì?"/>
+                                    <button type="submit">Tìm kiếm</button>
+                                </form>
+                            </div>
+                            <div className="hero_search_phone">
+                                <div className="hero_search_phone_icon">
+                                    <AiOutlinePhone/>
+                                </div>
+                                <div className="hero_search_phone_text">
+                                    <p>0396.665.399</p>
+                                    <span>Hỗ trợ 24/7</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="hero_item">
+                            <div className="hero_text">
+                                <span>Trái cây tươi</span>
+                                <h2>Rau quả <br/>
+                                    sạch 100%
+                                </h2>
+                                <p>Miễn phí giao hàng tận nơi</p>
+                                <Link to="" className="primary-btn">Mua ngay</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+       
+    )
 }
 
 export default memo(Header);
